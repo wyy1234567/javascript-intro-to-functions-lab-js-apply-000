53359bec610e70b446f29b67dd0727a7b22cab93
salubrious-commit-8954 <wangyy395@gmail.com>
2020-01-25 23:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowerCase = 'hello'
   
-  if (string === lowerCase) {
-    return ""
-  }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if 
 }
